fbe24c3acdd51d9ce55783e066a65aa7f669f8dc
harshitbansall <harshitbansal587@gmail.com>
2022-02-12 20:35:22 +0530

    Lecture 2

---
+++ Lecture 1/MyFirstClass.java
@@ -0,0 +1,6 @@
+class MyFirstClass{
+    public static void main(String[] args) {
+        System.out.print("Hello World.");
+
+    }
+}
---
+++ Lecture 2/Lecture2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Lecture2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input);
+
+    }
+}
