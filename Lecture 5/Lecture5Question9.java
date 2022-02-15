71ebe5da432312f76a612a399dd33b667fbe36f9
harshitbansall <harshitbansal587@gmail.com>
2022-02-16 03:11:12 +0530

    Test

--- Lecture 5/Lecture5Question9.java
+++ Lecture 5/Lecture5Question9.java
@@ -1,15 +1,10 @@
 public class Lecture5Question9 {
     public static void main(String[] args) {
-
         int totalRows = 10;
         for (int row = 1; row <= totalRows; row ++ ){
             for (int column = 1; column <= row; column++){
-                if ((row+column) % 2 == 0){
-                    System.out.print(1);
-                }
-                else{
-                    System.out.print(0);
-                }
+                if ((row+column) % 2 == 0){System.out.print(1);}
+                else{System.out.print(0);}
             }
             System.out.println();
         }
