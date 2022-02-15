7f0ede24aa9618a3f1f0e170224b8bec2e8cdb85
harshitbansall <harshitbansal587@gmail.com>
2022-02-15 18:23:41 +0530

    Lecture 5

---
+++ Lecture 5/Lecture5Question1.java
@@ -0,0 +1,10 @@
+public class Lecture5Question1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++){
+            for (int k = 1; k <= 5; k++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
---
+++ Lecture 5/Lecture5Question3.java
@@ -0,0 +1,12 @@
+public class Lecture5Question3 {
+    public static void main(String[] args) {
+        int totalRows = 10;
+        for (int row = 1; row <= totalRows; row ++ ){
+            for (int column = 1; column <= row; column++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
